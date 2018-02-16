8a81281213a28985ae2874ae8302fbec27f3a4af
janett <janett@janetts-MacBook-Pro.local>
2018-02-15 21:51:40 -0500

    adding new app

---
+++ index.js
@@ -0,0 +1,17 @@
+$('#zipcode')
+.click(function(){
+    var zipcodeValue = $('#weather').val()
+    var url = getApiUrl(zipcodeValue);
+    getWeatherData(url);
+})
+
+function getWeatherData(url){
+    $.get(url, function(data){
+        console.log(data);
+        $('#access').text(JSON.stringify(data.weather[0].description));
+    })
+}
+
+function getApiUrl(newZip){
+    return `http://api.openweathermap.org/data/2.5/weather?zip=${newZip}&APPID=af23f0fb6b37a45e1733df432b7737e6`;
+}
